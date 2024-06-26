b97c5e9deb36cc1b5df371a216ed7c52950e428b
Aditi Kumar <aditikumar2005@gmail.com>
2024-06-26 17:48:42 -0500

    working links

---
+++ beaches/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <a href="/test">About Page</a>
+    </div>
+  );
+};
+
+export default Page;
