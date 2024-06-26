690dafb40ce1f1dddea65160b7002ad4366ca8a1
Aditi Kumar <aditikumar2005@gmail.com>
2024-06-26 18:56:57 -0500

    map page

---
+++ beaches/components/Beaches.js
@@ -0,0 +1,15 @@
+import styles from "../styles/main.module.css";
+import React from "react";
+// import { filterData, getScore } from "../queries.js";
+// import Tag from "./Tag.js";
+// import TagContainer from "./TagContainer.js"
+
+
+export default function Lakes(name) {
+  return (
+    <>
+        <h2>NAME OF LAKE</h2>
+        
+    </>
+  );
+}
