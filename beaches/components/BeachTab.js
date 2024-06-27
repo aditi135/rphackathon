1f69535a46c2f1a4508ccabc6119a5a81dc318e6
Warren Lam <warrenklam@gmail.com>
2024-06-26 23:00:15 -0500

    main moduyle

--- beaches/components/BeachTab.js
+++ beaches/components/BeachTab.js
@@ -1,7 +1,8 @@
+// BeachTab.js
 import React, { useState } from 'react';
 import styles from './BeachTab.module.css';
 
-const BeachTab = ({ beach }) => {
+const BeachTab = ({ beach, onClick }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => {
@@ -9,25 +10,27 @@ const BeachTab = ({ beach }) => {
   };
 
   if (!beach) {
-    return null; // or return a loading placeholder
+    return null; // or handle loading state appropriately
   }
 
+  const handleTabClick = () => {
+    onClick(); // Trigger onClick passed from the parent component
+    toggleExpand(); // Optionally toggle expand state
+  };
+
   return (
-    <div className={styles.beachTab}>
-      <div 
-        className={styles.beachName} 
-        onClick={toggleExpand}
-      >
+    <div className={styles.beachTab} onClick={handleTabClick}>
+      <div className={styles.beachName}>
         {beach.name}
-        <span className={`${styles.arrow} ${isExpanded ? styles.up : styles.down}`}>
-          
-        </span>
+        <span className={`${styles.arrow} ${isExpanded ? styles.up : styles.down}`}></span>
       </div>
       <div className={`${styles.beachStats} ${isExpanded ? styles.expanded : ''}`}>
         <ul>
           <li>Water Temperature: {beach.statistics.waterTemperature}°F</li>
           <li>Wave Height: {beach.statistics.waveHeight} meters</li>
           <li>Crowdedness: {beach.statistics.crowdedness}/10</li>
+          <li>Predicted E.Coli Level: {beach.data.PredictedEcoliLevel} cfu /  100 ml</li>
+          <li>Predicted Enterococci Levels: {beach.data.PredictedEcoliLevel} cfu /  100 ml</li>
         </ul>
       </div>
     </div>
--- beaches/pages/_app.tsx
+++ beaches/pages/_app.tsx
@@ -1,4 +1,13 @@
+// import type { AppProps } from 'next/app'
+ 
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+
 import type { AppProps } from 'next/app'
+import '../styles/global.css';
+
  
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
